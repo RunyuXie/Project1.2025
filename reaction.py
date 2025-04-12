69e8a69515ea452afe03470cb4ff697979d2d162
RunyuXie <202483890027@nuist.edu.cn>
2025-04-12 20:06:01 +0800

    update code

--- reaction.py
+++ reaction.py
@@ -1,13 +1,22 @@
 from gpiozero import LED, Button
 from time import sleep
 from random import uniform
+
 led = LED(4)
 right_buttom = Buttom(15)
 left_buttom = Buttom(14)
+
+left_name = input('left player name is ')
+right_name = input('right player name is ')
+
 led.on()
 sleep(uniform(5, 10))
 led.off()
-def pressed(button):
-  print(str(button.pin.number) + ' won the game')
+
+if button.pin.number == 14:
+ print(left_name + ' won the game')
+else:
+ print(right_name + ' won the game')
+  
 right_button.when_pressed = pressed
 left_button.when_pressed = pressed
