20b18351a5561de189d9d5f49750762fc08cbe03
RunyuXie <202483890027@nuist.edu.cn>
2025-04-12 20:19:47 +0800

    add final task3

--- reaction.py
+++ reaction.py
@@ -9,14 +9,20 @@ left_buttom = Buttom(14)
 left_name = input('left player name is ')
 right_name = input('right player name is ')
 
+def pressed(button):
+    global start_time
+    reaction_time = time() - start_time
+    if button.pin.number == 14:
+        print(f"{left_name} won the game! Reaction time: {reaction_time:.3f} seconds")
+    else:
+        print(f"{right_name} won the game! Reaction time: {reaction_time:.3f} seconds")
+    exit()
+
 led.on()
 sleep(uniform(5, 10))
 led.off()
 
-if button.pin.number == 14:
- print(left_name + ' won the game')
-else:
- print(right_name + ' won the game')
+start_time = time()
   
 right_button.when_pressed = pressed
 left_button.when_pressed = pressed
